104da88df2f52c826f2f840793bb090f6c967e1a
Mac <145077500+MacDevelopment@users.noreply.github.com>
2025-06-26 12:18:56 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mac Development'
+description 'A /try command script for your FiveM server'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+server_scripts {
+    'server/server.lua'
+}
+shared_script 'shared/config.lua'
